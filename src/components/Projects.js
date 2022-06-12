c6c8302a7f7adee77eaf22c9ce36b7061c3b49c0
vedri45 <vedri45@gmail.com>
2022-06-12 15:29:07 +0700

    update project manga to card

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,20 +7,20 @@ export default class Projects extends React.Component {
         {
             name: 'Asia Mega Pasifik',
             url: 'https://asiamegapasifik.com/',
-            code: 'wordpress',
+            code: 'Wordpress',
             image: 'project_sample_1.png'
         },
         {
             name: 'Storm Age Capital Fund',
             url: 'https://sacfglo.com/',
-            code: 'php',
+            code: 'PHP',
             image: 'project_sample_2.png'
         },
         {
-            name: 'Card 3',
-            url: 'https://example.com/',
-            code: 'css',
-            image: 'placeholder-image.png'
+            name: 'Manga Database App',
+            url: 'https://manga-database-app.netlify.app/',
+            code: 'React JS',
+            image: 'project_sample_3.png'
         },
     ];
 
@@ -30,22 +30,20 @@ export default class Projects extends React.Component {
                 <Container>
                     <Row>
                         <h2 className="py-2 text-light text-center">Projects</h2>
-                        {this.web.map(item => {
-                            return (
-                                <Col sm={12} md={12} lg={4} className="d-flex">
-                                    <Card style={{ width: '18rem' }} className="mx-auto my-3 ">
-                                        <Card.Img variant="top" src={item.image} />
-                                        <Card.Body className="bg-dark text-light">
-                                            <Card.Title>{item.name}</Card.Title>
-                                            <Card.Text>
-                                                This project was built with {item.code}
-                                            </Card.Text>
-                                            <Button variant="primary" href={item.url} target="_blank">Go somewhere</Button>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-                            )
-                        })}
+                        {this.web.map((item, i) => (
+                            <Col sm={12} md={12} lg={4} className="d-flex" key={i}>
+                                <Card style={{ width: '18rem' }} className="mx-auto my-3">
+                                    <Card.Img variant="top" src={item.image} />
+                                    <Card.Body className="bg-dark text-light">
+                                        <Card.Title>{item.name}</Card.Title>
+                                        <Card.Text>
+                                            This project was built with {item.code}
+                                        </Card.Text>
+                                        <Button variant="primary" href={item.url} target="_blank">Go somewhere</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        ))}
                     </Row>
                 </Container>
             </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,23 +4,21 @@ import { faCheck } from '@fortawesome/free-solid-svg-icons'
 import { Col, Container, ListGroup, Row } from 'react-bootstrap'
 
 export default class Skills extends Component {
-    code = ['HTML', 'CSS', 'JavaScript', 'PHP', 'Bootstrap', 'React']
+    code = ['HTML', 'CSS', 'JavaScript', 'PHP', 'Bootstrap', 'React JS', 'jQuery', 'Wordpress']
     render() {
         return (
             <div id="skills">
                 <Container>
                     <Row>
                         <h2 className="py-2 text-light text-center">Skills</h2>
-                        {this.code.map(codes => {
-                            return (
-                                <Col sm={12} md={6}>
-                                    <ListGroup className="text-start">
-                                        <ListGroup.Item className="bg-dark text-white my-2">
-                                            <FontAwesomeIcon icon={faCheck} style={{ color: 'green' }} /> {codes}</ListGroup.Item>
-                                    </ListGroup>
-                                </Col>
-                            )
-                        })}
+                        {this.code.map((codes, i) => (
+                            <Col sm={12} md={6} key={i}>
+                                <ListGroup className="text-start">
+                                    <ListGroup.Item className="bg-dark text-white my-2">
+                                        <FontAwesomeIcon icon={faCheck} style={{ color: 'green' }} /> {codes}</ListGroup.Item>
+                                </ListGroup>
+                            </Col>
+                        ))}
                     </Row>
                 </Container>
             </div>
